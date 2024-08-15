de9ed3c9e660b7175b946ad03e131fe1cc2b16d4
nikhil <nikhilmahato3337@gmail.com>
2024-08-15 10:03:39 +0530

    static

--- server.js
+++ server.js
@@ -10,9 +10,16 @@ import { authMiddleware } from "./middlewares/authMiddleware.js";
 import classroomRouter from "./routes/classroomRoutes.js";
 import userRouter from "./routes/userRoutes.js";
 
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+app.use(express.static(path.resolve(__dirname, "./public")));
 
 app.use(cookieParser());
 app.use(express.json());
@@ -24,6 +31,10 @@ app.use("/api/auth", authRouter);
 app.use('/api/classrooms', authMiddleware, classroomRouter);
 app.use('/api/users',authMiddleware, userRouter);
 
+
+app.get('*',(req,res)=>{
+  res.sendFile(path.resolve(__dirname,'./public','index.html'))
+})
 
 app.use("*", (req, res) => {
   res.status(404).json({ msg: "not found" });
